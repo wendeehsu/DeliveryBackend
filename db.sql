6b6a7d26a621cdeef254b090d79087aaf22c4916
Melhor <b05705046@ntu.edu.tw>
2019-12-27 00:40:45 +0800

    create platform_Activities

--- db.sql
+++ db.sql
@@ -4,10 +4,24 @@ USE delivery_db;
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-  id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  uid int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(255),
   phone VARCHAR(255),
   address VARCHAR(255),
   password VARCHAR(255) NOT NULL,
   account VARCHAR(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
+
+INSERT INTO users (name,phone,address,password,account) values
+("adam",NULL,NULL,"test1234","testacct"),
+("vivian","091234",NULL,"vivi1111","vivi");
+
+DROP TABLE IF EXISTS platform_Activities;
+CREATE TABLE platform_Activities (
+  pid int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  discount_shippingFee int(11) NOT NULL,
+  discountMode int NOT NULL,
+  discountParam float,
+  description VARCHAR(255),
+  pic_url VARCHAR(255)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
